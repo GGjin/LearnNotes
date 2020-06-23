e4108fab8114ea434dced8538fe0170b358b0144
GGjin <gg.jin.yu@gmail.com>
2020-06-23 17:09:09 +0800

    Initial commit

---
+++ basicgrammar.py
@@ -0,0 +1,30 @@
+a = "this " "is " "string"
+
+print(10 ** 3)
+
+b = True
+
+if b != False:
+    print(b)
+else:
+    print("---")
+
+aa = 'noop'
+
+print(aa[2::1])
+
+# print(aa)  # 输出字符串
+# print(aa[0:-1])  # 输出第一个到倒数第二个的所有字符
+# print(aa[0])  # 输出字符串第一个字符
+# print(aa[2:5])  # 输出从第三个开始到第五个的字符
+# print(aa[2:])  # 输出从第三个开始后的所有字符
+# print(aa * 2)  # 输出字符串两次
+# print(aa[-1::1])
+#
+# print(aa[::-1])
+
+# input(r"\n\n按下 enter 键后退出。")
+
+c = 345
+
+print((str(c))[::-1])
